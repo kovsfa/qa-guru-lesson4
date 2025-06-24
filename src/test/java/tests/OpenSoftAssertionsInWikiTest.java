5e57a1102fd9796b971b5c6503ab6b1b68c25d1c
Sofia Kovaleva <kov.sfa@gmail.com>
2025-06-24 23:12:40 +0500

    added file: open softAssertions and find code for JUnit5

---
+++ src/test/java/tests/OpenSoftAssertionsInWikiTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OpenSoftAssertionsInWikiTest {
+    @Test
+    void openSoftAssertionsInWiki() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#js-repo-pjax-container").shouldHave(text("Soft assertions"));
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        $("#repo-content-pjax-container").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
