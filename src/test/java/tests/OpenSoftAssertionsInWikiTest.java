4c2ba7af1dc887627deea8b2ef5c351c3ec10b36
Sofia Kovaleva <kov.sfa@gmail.com>
2025-06-24 23:30:44 +0500

    added v2 of OpenSoftAssertionsInWikiTest

--- src/test/java/tests/OpenSoftAssertionsInWikiTest.java
+++ src/test/java/tests/OpenSoftAssertionsInWikiTest.java
@@ -7,22 +7,24 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class OpenSoftAssertionsInWikiTest {
+    String JUnit5CodeExample = "@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}";
     @Test
     void openSoftAssertionsInWiki() {
+
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("#js-repo-pjax-container").shouldHave(text("Soft assertions"));
         $("#wiki-body").$(byText("Soft assertions")).click();
-        $("#repo-content-pjax-container").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-                "class Tests {\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }\n" +
-                "}"));
+        $("#repo-content-pjax-container").shouldHave(text(JUnit5CodeExample));
     }
 }
